2b42b3b35369a123394768d806a9eb686df6699d
Quirius <szogever@gmail.com>
2026-01-17 16:11:46 +0100

    v0.8 - added pygame groups

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
@@ -20,8 +23,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
